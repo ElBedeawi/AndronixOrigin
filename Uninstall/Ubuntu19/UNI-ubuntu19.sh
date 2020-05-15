d51b7a4eb705827e4f9bd42c68237c03023ed5c9
wagih elbedeawi <wagih.elbedeawi@gmail.com>
2020-05-15 16:46:45 +0200

    u20-gnome test#3

--- Uninstall/Ubuntu19/UNI-ubuntu19.sh
+++ Uninstall/Ubuntu19/UNI-ubuntu19.sh
@@ -3,10 +3,15 @@
 echo "Starting to uninstall, please be patient..."
 
 chmod 777 -R ubuntu19-fs
+chmod 777 -R ubuntu20-fs
 rm -rf ubuntu19-fs
+rm -rf ubuntu20-fs
 rm -rf ubuntu19-binds
+rm -rf ubuntu20-binds
 rm -rf ubuntu19.sh
+rm -rf ubuntu20.sh
 rm -rf start-ubuntu19.sh
+rm -rf start-ubuntu20.sh
 rm -rf de-apt-xfce4.sh
 rm -rf de-apt-mate.sh
 rm -rf de-apt-lxqt.sh
